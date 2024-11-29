b7cb919810fddcb9bb722a2ad1a0e0eef8e17daa
andreamagan <andreamr@ext.zara.com>
2024-11-29 10:35:01 +0100

    move site into docs

--- _config.ts
+++ _config.ts
@@ -1,7 +1,10 @@
 import lume from "lume/mod.ts";
 import top from "top/mod.ts";
 
-const site = lume();
+const site = lume({
+  src: "./", // Directorio de origen
+  dest: "./docs", // Directorio de destino
+});
 
 site.use(top());
 
